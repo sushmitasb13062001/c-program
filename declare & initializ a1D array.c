ec885844b56fbf717f0f45f200747237a946a42e
sushmitasb13062001 <sushmitasb13062001@gmail.com>
2025-12-10 08:38:47 -0800

    Add files via upload

---
+++ declare & initializ a1D array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int arr[8] = {1,2,3,4,5,6,7,8};
+    
+    printf("the elements are:\n");
+    for(int i=0;i<8;i++){
+        printf("%d\n",arr[i]);
+    }
+    return 0;
+}
