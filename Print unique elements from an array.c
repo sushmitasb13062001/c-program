d6bbfb87588725ad1a64eb30d637ddb672bf0d37
sushmitasb13062001 <sushmitasb13062001@gmail.com>
2025-12-21 09:03:53 -0800

    Add files via upload

---
+++ Print unique elements from an array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+   int arr[100],i,j,n,count;
+   
+   printf("enter the number of elements:");
+   scanf("%d",&n);
+   
+   printf("enter the numbers:");
+   for(i=0;i<n;i++){
+       scanf("%d",&arr[i]);
+   }
+   
+   for(i=0;i<n;i++){
+       count = 0;
+       
+       for(j=0;j<n;j++){
+           if(arr[i]==arr[j]){
+               count++;
+           }
+       }
+       
+       if(count==1){
+           printf("%d",arr[i]);
+       }
+   }
+    return 0;
+}
